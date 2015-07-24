3ac3736070a3496128cd9e6eed68c3c32fbb66e9
Chris MacDonald <cmacdonald@pbs.org>
2015-07-24 16:16:30 -0400

    better delivery data, image service

---
+++ src/js/data-services/ImageServiceAPI.js
@@ -0,0 +1,11 @@
+var q = require('q');
+var imageURLS = require('../../data/image_urls')
+
+// Wikimedia Commons, I ask your forgiveness.
+var NelsoniAPI = {
+  getImageURL: function(image_name) {
+    return imageURLS.imagename
+  }
+}
+
+module.exports = NelsoniAPI
--- src/js/react-components/Delivery.js
+++ src/js/react-components/Delivery.js
@@ -2,13 +2,13 @@ var React = require('react');
 
 var Delivery = React.createClass({
   render: function() {
-      return (
-        <div className="panel panel-default">
-          <div className="panel-heading">A Delivery</div>
-          <div className="panel-body">
-            data data
-          </div>
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">A Delivery</div>
+        <div className="panel-body">
+          data data
         </div>
+      </div>
     );
   }
 })
