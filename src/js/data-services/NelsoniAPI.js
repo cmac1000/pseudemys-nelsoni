dc299796c1b13a95241c1e32bcbdd51058f2ccdb
Chris MacDonald <cmac1000@gmail.com>
2015-07-25 11:51:35 -0400

    complexify

--- src/js/data-services/NelsoniAPI.js
+++ src/js/data-services/NelsoniAPI.js
@@ -1,14 +1,30 @@
 var q = require('q');
 var schedules = require('../../data/schedules')
 var orders = require('../../data/orders')
+var imageUrls = require('../../data/image_urls')
 
 var NelsoniAPI = {
   getSchedules: function() {
-    return q.resolve(schedules)
+    return q.resolve(
+      schedules.map(function(schedule) {
+        schedule.deliveries = schedule.deliveries.map(function (delivery) {
+          if(!imageUrls[delivery['order']['destination']]) {
+            throw new Error('unknown destination')
+          }
+          if(!imageUrls[delivery['order']['turtle_type']]) {
+            throw new Error('unknown turtle type')
+          }
+          delivery.moonImgURL = imageUrls[delivery['order']['destination']];
+          delivery.turtleImgURL = imageUrls[delivery['order']['turtle_type']];
+          return delivery;
+        })
+        return schedule;
+      })
+    );
   },
   getOrders: function() {
-    return q.resolve(orders)
+    return q.resolve(orders);
   }
 }
 
-module.exports = NelsoniAPI
+module.exports = NelsoniAPI;
--- src/js/react-components/Delivery.js
+++ src/js/react-components/Delivery.js
@@ -2,11 +2,29 @@ var React = require('react');
 
 var Delivery = React.createClass({
   render: function() {
+    var self = this;
+    var data = self.props.data;
     return (
       <div className="panel panel-default">
-        <div className="panel-heading">A Delivery</div>
+        <div className="panel-heading">
+          {data.get('order').get('turtle_quantity') + " "}
+          <b>
+            {data.get('order').get('turtle_type')}
+          </b>
+          {" to " + data.get('order').get('destination')}
+        </div>
         <div className="panel-body">
-          data data
+          <div className="row">
+            <div className="col-md-4">
+              <img src={data.get('turtleImgURL')} style={{width: "50%"}}></img>
+            </div>
+            <div className="col-md-4">
+              {" => "}
+            </div>
+            <div className="col-md-4">
+              <img src={data.get('moonImgURL')} style={{width: "50%"}}></img>
+            </div>
+          </div>
         </div>
       </div>
     );
